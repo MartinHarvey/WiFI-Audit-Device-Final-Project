945d6763150cc4fbf1f0a1c452b4988d68111330
MartinHarvey <martin.harvey.2017@uni.strath.ac.uk>
2021-02-12 16:35:39 +0000

    Fixed problem with network-list.py

--- code/network_list.py
+++ code/network_list.py
@@ -4,9 +4,11 @@ import sys
 def main(args):
     try:
         output = open(args[1], 'w')
+        sys.stdout = output
     except IndexError:
         output = None
-    subprocess.run(["nmcli","-f", "BSSID,SSID,CHAN,SIGNAL,SECURITY" ,"dev", "wifi"], stdout=output)
+    proc = subprocess.Popen(["nmcli","-f", "BSSID,SSID,CHAN,SIGNAL,SECURITY" ,"dev", "wifi"], stdout=subprocess.PIPE)
+    print(proc.stdout.read().decode())
 
 if __name__ == "__main__":
     main(sys.argv)
