5cc60701d3205fb1147cb26e34aed1d7b79eec4a
MartinHarvey <martin.harvey.2017@uni.strath.ac.uk>
2021-02-12 12:26:48 -0500

    Readded code that lists bluetooth devices. Dissapeared in a recent commit, must have been deleted by accident

--- code/bluetooth_list.py
+++ code/bluetooth_list.py
@@ -9,9 +9,11 @@ def main(args):
 		pass
 	try:
 		devices = bluetooth.discover_devices(lookup_names=True)
+		print("Address           Name") 
+		for addr, name in devices:
+			print(addr + " " + name)
 	except OSError:
 		print("No bluetooth hardware to use")
-		devices = []
 
 if __name__ == "__main__":
 	main(sys.argv)
